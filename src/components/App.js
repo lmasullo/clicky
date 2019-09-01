e4065ad25d6b9c8033e1d3ade28f01d4f4248a3a
Lawrence N. Masullo <lmasullo@gmail.com>
2019-09-01 14:41:43 -0500

    Looping over cards

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable react/no-access-state-in-setstate */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable prefer-destructuring */
@@ -5,74 +6,89 @@ import React from 'react';
 // import logo from './logo.svg';
 // import '../css/App.css';
 
+// Import the Components
 import Header from './Header';
 import Instructions from './Instuctions';
 import Cards from './Cards';
 import Footer from './Footer';
 
+// Create the array cards
 const cards = [
   {
+    id: 1,
     name: 'beth',
     image: '../images/beth.png',
     isClicked: false,
   },
   {
+    id: 2,
     name: 'birdperson',
     image: '../images/birdperson.png',
     isClicked: false,
   },
   {
+    id: 3,
     name: 'evilmorty',
     image: '../images/evilmorty.png',
     isClicked: false,
   },
   {
+    id: 4,
     name: 'gianthead',
     image: '../images/gianthead.png',
     isClicked: false,
   },
   {
+    id: 5,
     name: 'goldenford',
     image: '../images/goldenford.png',
     isClicked: false,
   },
   {
+    id: 6,
     name: 'jerry',
     image: '../images/jerry.png',
     isClicked: false,
   },
   {
+    id: 7,
     name: 'jessica',
     image: '../images/jessica.png',
     isClicked: false,
   },
   {
+    id: 8,
     name: 'meeseeks',
     image: '../images/meeseeks.png',
     isClicked: false,
   },
   {
+    id: 9,
     name: 'morty',
     image: '../images/morty.png',
     isClicked: false,
   },
   {
+    id: 10,
     name: 'mr',
     image: '../images/mr.png',
     isClicked: false,
   },
   {
+    id: 11,
     name: 'rick',
     image: '../images/rick.png',
     isClicked: false,
   },
   {
+    id: 12,
     name: 'summer',
     image: '../images/summer.png',
     isClicked: false,
   },
 ];
 
+// Set the initial Sate
 class App extends React.Component {
   state = {
     score: 0,
@@ -80,14 +96,18 @@ class App extends React.Component {
     cards,
   };
 
+  // Function to update the Score/Topscore State
+  // Use this syntax to get the info from the child component
   updateScore = scores => {
     console.log('Update score');
     // Take a copy of existing state
     let score = { ...this.state.score };
     let topscore = { ...this.state.topscore };
-    // Add new score and topscore to variable
+
+    // Add new score and topscore to a variable
     score = scores.score;
     topscore = scores.topscore;
+
     // Set new score
     this.setState({
       score,
@@ -95,17 +115,73 @@ class App extends React.Component {
     });
   };
 
+  // Function to update the isClicked on the card object
   updateClicked = clicked => {
-    const cards = { ...this.state.cards };
-    cards[0].isClicked = clicked;
-    this.setState({
-      cards,
-    });
+    console.log('Update Clicked');
+    // Take a copy
+    // const cards = { ...this.state.cards };
+
+    console.log(typeof cards);
+
+    // const filteredCard = cards.filter(i => i.includes(clicked.id));
+
+    // const filteredCard = cards.filter(clicked.id);
+    console.log(`Clicked ID: ${clicked.id}`);
+
+    // console.log(typeof clicked.id);
+
+    const intClicked = parseInt(clicked.id);
+
+    // Add new value
+
+    // Loop through array
+    // Filter card to the clicked id
+
+    for (let i = 0; i < cards.length; i++) {
+      console.log(cards[i]);
+      if (cards[i].id === intClicked) {
+        console.log('Match in for loop');
+
+        // Take a copy
+        const cards = { ...this.state.cards };
+
+        // Set value
+        cards[i].isClicked = true;
+
+        // Set state
+        this.setState({ cards });
+      }
+    }
+
+    // Object.keys(cards).forEach(key => {
+    //   if (cards[key].id === intClicked) {
+    //     console.log('Match');
+
+    //     console.log(cards[key].index);
+
+    //     console.log(
+    //       `ID: ${cards[key].id}, Index: , Name: ${cards[key].name}, isClicked: ${cards[key].isClicked}`
+    //     );
+
+    //     cards[0].isClicked = true;
+    //   }
+    // });
+
+    // Set state
+    // this.setState({ cards });
+
+    // cards[0].isClicked = clicked;
+    // this.setState({
+    // cards,
+    // });
   };
 
+  // React Render function
   render() {
+    // Return Components/HTML to the index.html root div, uses index.js
     return (
       <div className="App">
+        {/* Send state down to child components */}
         <Header score={this.state.score} topscore={this.state.topscore} />
         <Instructions />
         <Cards
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,6 +6,7 @@
 /* eslint-disable class-methods-use-this */
 import React from 'react';
 
+// Import css for cards.js
 import '../css/cards.css';
 
 // Inline CSS
@@ -23,6 +24,7 @@ const styleCont = {
   justifyContent: 'center',
 };
 
+// Function that adds the shake class to all images for .5 seconds, then removes
 function shake() {
   const allImgs = document.querySelectorAll('.img-thumbnail');
   for (let i = 0; i < allImgs.length; i++) {
@@ -36,30 +38,58 @@ function shake() {
   }, 500);
 }
 
+// Create object to hold the scores to return
 const scores = {
   score: 0,
   topscore: 0,
 };
 
 class Cards extends React.Component {
-  // handleClick = event => {
-  handleClick(e) {
+  // Use this syntax so we have access to 'this'
+  handleClick = event => {
+    console.log(this);
+
+    // Create an empty Ref
+    // const myClick = React.createRef();
+
+    // handleClick(event) {
     // const newScore = 5;
     // const topscore = 10;
 
-    console.log(this);
+    // console.log(this);
 
     // Check if has been clicked
-    // console.log(e.currentTarget.dataset.isclicked);
-    const isClicked = e.currentTarget.dataset.isclicked;
-    // todo Send the array index this with clixked in an object
+    // console.log(event.currentTarget.dataset.isclicked);
+
+    // Get the id of the image clicked
+    const isClickedID = event.currentTarget.id;
+
+    // Get the value of isclicked
+    const isClicked = event.currentTarget.dataset.isclicked;
+    console.log(isClickedID);
+    console.log(isClicked);
+    // const isClickedID = event.currentTarget.dataset.id;
+
+    // console.log(isClicked, isClickedID);
+
+    // Create the object to hold the response to send to updateClicked
+    const clicked = {
+      isClicked,
+      id: isClickedID,
+    };
+
+    // const { isClicked } = this.props;
+    // todo Send the array index this with clicked in an object
     // If not previously clicked, set as clicked, increment score,
     // if score > top score, increment top score, and shuffle
     if (isClicked === 'false') {
-      console.log('False');
+      console.log('Its False');
       //! Set State??
       // todo make isClicked = true
-      this.props.updateClicked(true);
+
+      // Set the response object
+      clicked.isClicked = true;
+      this.props.updateClicked(clicked);
 
       scores.score++;
       console.log(scores.score);
@@ -75,33 +105,31 @@ class Cards extends React.Component {
       // Call Shake
       shake();
     }
-  }
+  };
 
   render() {
     console.log(this.props.cards);
+
+    // Get the cards array from props
     const { cards } = this.props;
 
+    // Return this App.js
     return (
       <>
-        {/* <button
-          type="button"
-          // onClick={() => {
-          //   this.handleClick(cards);
-          // }}
-          // onClick={this.handleClick}
-        >
-          Shake
-        </button> */}
         <div className="container" style={styleCont}>
+          {/* Map over the cards arrays */}
           {cards.map((value, index) => (
+            // Create the img tags
             <img
               key={index}
+              id={cards[index].id}
               alt={cards[index].name}
               src={cards[index].image}
               data-isclicked={cards[index].isClicked}
+              // ref={cards[index].isClicked}
               className="img-thumbnail"
               style={styles}
-              onClick={e => this.handleClick(e)}
+              onClick={event => this.handleClick(event)}
             />
           ))}
         </div>
