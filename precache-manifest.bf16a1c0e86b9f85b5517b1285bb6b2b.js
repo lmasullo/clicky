79551023860460a9c692f24625bf5064916e23d5
Lawrence N. Masullo <lmasullo@gmail.com>
2019-09-04 09:22:50 -0500

    Updates

---
+++ precache-manifest.bf16a1c0e86b9f85b5517b1285bb6b2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05f64e9938e5ab0665744cab8a47b610",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "c08dbb021c6061bd4ba2",
+    "url": "/clicky/static/css/main.5344c0ce.chunk.css"
+  },
+  {
+    "revision": "515c38e9a345c80ac37a",
+    "url": "/clicky/static/js/2.87582209.chunk.js"
+  },
+  {
+    "revision": "c08dbb021c6061bd4ba2",
+    "url": "/clicky/static/js/main.32eb39d8.chunk.js"
+  },
+  {
+    "revision": "9d378f700a36f547cf37",
+    "url": "/clicky/static/js/runtime~main.68383fad.js"
+  }
+]);
